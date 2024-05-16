3250cf70f45f1737a89a82a444a045583adc04dd
Caroline Conte <carolinecontee@gmail.com>
2024-05-16 15:54:09 +0200

    abertura

--- src/components/layout/TopPage.tsx
+++ src/components/layout/TopPage.tsx
@@ -10,6 +10,50 @@ type Props = {}
 
 const TopPage = (props: Props) => {
 
+  const [statusAbertura, setStatusAbertura] = useState('')
+
+  useEffect(() => {
+    let agora = new Date();
+    let diaSemana = agora.getDay(); // Retorna o dia da semana (0 = Domingo, 1 = Segunda, ..., 6 = Sábado)
+    let horasMS = agora.getHours();
+    let statusAbertura = '';
+  
+    // Verifica se é sábado ou domingo
+    if (diaSemana === 0 || diaSemana === 6) {
+      statusAbertura = 'Fechado';
+    } else {
+      // Define os horários de abertura e fechamento
+      let hrAbreManha = 8;
+      let hrFechaManha = 11;
+      let hrAbreTarde = 13;
+      let hrFechaTarde = 18;
+  
+      // Verifica o horário atual
+      if ((horasMS >= hrAbreManha && horasMS < hrFechaManha) || (horasMS >= hrAbreTarde && horasMS < hrFechaTarde)) {
+        statusAbertura = 'Aberto';
+      } else {
+        let horasFaltando = 0;
+  
+        // Se a hora atual for depois do horário de fechamento da manhã, calcula as horas restantes até o horário de abertura da tarde
+        if (horasMS >= hrFechaManha && horasMS < hrAbreTarde) {
+          horasFaltando = hrAbreTarde - horasMS;
+        }
+        // Se a hora atual for depois do horário de fechamento da tarde, calcula as horas restantes até o horário de abertura do próximo dia
+        else if (horasMS >= hrFechaTarde) {
+          horasFaltando = (24 - horasMS) + hrAbreManha;
+        }
+        // Se a hora atual for antes do horário de abertura da manhã, calcula as horas restantes até o horário de abertura
+        else {
+          horasFaltando = hrAbreManha - horasMS;
+        }
+  
+        statusAbertura = `Fechado, abre em: ${horasFaltando} horas`;
+      }
+    }
+  
+    setStatusAbertura(statusAbertura);
+  }, []);
+  
   return (
     <section className="text-white w-full flex-between-center flex-wrap gap-1 text-[.7em] p-2 
     sm:px-12 sm:py-3 sm:relative lg:text-[1.1em]">
@@ -18,7 +62,9 @@ const TopPage = (props: Props) => {
         alt="Logo" className="hidden sm:block lg:w-[200px]"
       />
       <div className="flex-between-center gap-6 grow sm:justify-end group">
- 
+        {statusAbertura && <p className={`${statusAbertura === 'Aberto' ? 'text-green-700' : ''} font-bold `}>
+          {`${statusAbertura}`}
+        </p>}
         <div className="flex gap-2 items-center sm:items-end">
           <Phone className='hidden sm:block group-hover:animate-pulse' />
           <Link href={''} aria-label="Ligar para clinica">
