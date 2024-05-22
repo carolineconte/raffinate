4bb06f4dd958975ed7c678fa870591a6f621dbf4
Caroline Conte <carolinecontee@gmail.com>
2024-05-22 09:42:22 +0200

    hover button

--- src/components/PrimaryBtn.tsx
+++ src/components/PrimaryBtn.tsx
@@ -6,7 +6,7 @@ const PrimaryBtn = (props: Props) => {
   return (
     <Link href='http://wa.me/556740429082' target="_blank" aria-label="Agendar Consulta" id='Agendar Consulta'
       className="shadow-btn	text-white-mist bg-blue-black p-4 rounded-2xl font-medium uppercase text-xs 
-       w-fit m-auto lg:text-xl transition-all hover:scale-105 hover:bg-blue-black/80">
+       w-fit m-auto lg:text-xl transition-all hover:scale-105 hover:bg-[#08090D]">
       Agende agora sua consulta
     </Link>
   )
