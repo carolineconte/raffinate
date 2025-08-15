07f499c1674c89b2349c3f5f50bfe735b7e16dfd
Caroline Conte <carolinecontee@gmail.com>
2025-08-15 08:39:39 +0200

    google script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Public_Sans } from "next/font/google";
+import Script from 'next/script'
 
 import "./globals.css";
 
@@ -17,8 +18,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17439030146"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17439030146');
+          `}
+        </Script>
+      </head>
       <body className={publicSans.className}>
-          {children}
+        {children}
       </body>
     </html>
   );
